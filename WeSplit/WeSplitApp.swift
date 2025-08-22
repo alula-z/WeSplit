9bcfbaa135e7807740aadc3843a1f7b16804bd47
alula-z <azeruesenay14@gmail.com>
2025-08-22 00:18:40 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Alula Zeruesenay on 8/22/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
